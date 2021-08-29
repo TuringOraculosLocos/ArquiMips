09d660c858bcb647b32423d57515c82c8972964a
Ricardo Taboada Magallanes <rtaboada@lenovo-ideapad-110>
2021-08-29 18:18:18 -0500

    [ADDED] Skeleton for accesing to register
    I've created a simple class to store the concept of access to register
    This could change in the future.

--- src/simulator/registers.py
+++ src/simulator/registers.py
@@ -5,3 +5,11 @@
 # Nombre: Erick Ivan Perez Jimenez
 # No.Cuenta: 311042122
 
+
+REGISTER_LENGTH_NUM = 14
+EMPTY_WORD = [ b'\x00' for _ in range (4)]
+
+class MIPSRegister(object):
+    def __init__(self) -> None:
+        super().__init__()
+        self.__mips_bytes = [ EMPTY_WORD for _ in range(REGISTER_LENGTH_NUM)] 
